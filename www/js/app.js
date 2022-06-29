2b8bd2bd48d86d22bfaa9639a83a4b66a187dc8f
Roberto Hebert <hebert.souzavinha@live.com>
2022-06-29 15:32:12 -0300

    Commit do Projeto RepApp
    Commit do Projeto RepApp, aplicativo para repúblicas.

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,169 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('agendaApp', ['ionic', 'firebase']);
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  // Tela "Lista"
+  $stateProvider.state('lista', {
+    url: '/lista',
+    templateUrl: 'templates/lista.html',
+    controller: 'ListaCtrl'
+  });
+
+   // Tela "Login"
+  $stateProvider.state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  });
+
+    // Tela "Menu"
+    $stateProvider.state('menu', {
+      url: '/menu',
+      templateUrl: 'templates/menu.html',
+      controller: 'MenuCtrl'
+    });
+
+      // Tela "Cadastro"
+  $stateProvider.state('cadastro', {
+    url: '/cadastro',
+    templateUrl: 'templates/cadastro.html',
+    controller: 'CadastroCtrl'
+  });
+
+  // Tela "Cadastro de República"
+  $stateProvider.state('cadastro_republica', {
+    url: '/cadastro_republica',
+    templateUrl: 'templates/cadastro_republica.html',
+    controller: 'CadastroRepublicaCtrl'
+  });
+
+    // Tela "Adicionar Afazer"
+    $stateProvider.state('adicionar_afazer', {
+      url: '/adicionar_afazer',
+      templateUrl: 'templates/adicionar_afazer.html',
+      controller: 'AddAfazerCtrl'
+    });
+
+    // Tela "Adicionar Conta"
+    $stateProvider.state('adicionar_conta', {
+      url: '/adicionar_conta',
+      templateUrl: 'templates/adicionar_conta.html',
+      controller: 'AddContaCtrl'
+    });
+
+    // Tela "Dados Pessoais"
+    $stateProvider.state('dados_pessoais', {
+      url: '/dados_pessoais',
+      templateUrl: 'templates/dados_pessoais.html',
+      controller: 'DadosPessoaisCtrl'
+    });
+
+  // Indicar a tela inicial (Initial State)
+  $urlRouterProvider.otherwise('/login');
+
+});
+
+//Controlador da lista
+app.controller("ListaCtrl", function($scope, $state) {
+
+  $scope.menu = function() {
+    $state.go('menu');
+
+  }
+});
+
+//Controlador do cadastro
+app.controller('CadastroCtrl', function($scope, $state) {
+
+});
+
+//Controlador do cadastro da república
+app.controller('CadastroRepublicaCtrl', function($scope, $state) {
+
+  $scope.addRepublica = function() {
+    $state.go('cadastro_republica');
+    }
+
+});
+
+//Controlador do login
+app.controller('LoginCtrl', function($scope, $state) {
+  
+ $scope.entrar = function() {
+  $state.go('lista');
+  }
+
+  $scope.addMorador = function() {
+    $state.go('cadastro');
+  }
+
+});
+
+//Controlador do menu
+app.controller('MenuCtrl', function($scope, $state) {
+  
+  $scope.addMorador = function() {
+    $state.go('cadastro');
+  }
+
+  $scope.addRepublica = function() {
+    $state.go('cadastro_republica');
+  }
+
+  $scope.addAfazer = function() {
+    $state.go('adicionar_afazer');
+  }
+
+  $scope.addConta = function() {
+    $state.go('adicionar_conta');
+  }
+
+  $scope.dadosPessoais = function() {
+    $state.go('dados_pessoais');
+  }
+
+});
+
+//Controlador Tela Adicionar Afazer
+app.controller('AddAfazerCtrl', function($scope, $state) {
+
+
+});
+
+//Controlador Tela Adicionar Conta
+app.controller('AddContaCtrl', function($scope, $state) {
+  
+});
+
+//Controlador Tela Adicionar Conta
+app.controller('DadosPessoaisCtrl', function($scope, $state) {
+  
+});
+
+//Controlador de Afazeres
+app.controller('AfazeresCtrl', function($scope, $state, $firebaseArray) {
+  var ref = firebase.database().ref('afazeres')
+  $scope.afazeres = $firebaseArray(ref);
+});
+
+app.factory('AfazerService', function() {
+
+});
+
+// Para cada tela, temos que criar:
+// 1. HTML (dentro de templates)
+// 2. O Controller
+// 3. O Estado ($State) 
